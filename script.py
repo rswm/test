e4cc8a55faa8bf9a25e40efa281b4ad2e31093a4
= <rswmallett@gmail.com>
2024-01-09 14:39:12 +0000

    Add addition function

--- script.py
+++ script.py
@@ -2,7 +2,9 @@ def multiply(a:int, b:int) -> int:
     """Mult"""
     return a * b
 
+def addition(a:int, b:int) -> int:
+    return a + b
+
 if __name__ == '__main__':
     print(multiply(6, 7))
     
-dfsdf
